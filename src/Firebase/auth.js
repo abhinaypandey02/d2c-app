783049e70b6ed849e2e89428b2596936e1e53e01
Abhinay Pandey <abhinaypandey02@gmail.com>
2022-04-10 01:28:14 +0530

    changes

--- src/Firebase/auth.js
+++ src/Firebase/auth.js
@@ -1,6 +1,30 @@
-import auth from "@react-native-firebase/auth";
+import auth, {firebase} from "@react-native-firebase/auth";
 
+const firebaseConfig = {
+
+    apiKey: "AIzaSyDuXOGANdHY4QHl-0ChxAuAFouzGZwiSqc",
+
+    authDomain: "freshtables-5ca16.firebaseapp.com",
+
+    databaseURL: "https://freshtables-5ca16-default-rtdb.firebaseio.com",
+
+    projectId: "freshtables-5ca16",
+
+    storageBucket: "freshtables-5ca16.appspot.com",
+
+    messagingSenderId: "893322883008",
+
+    appId: "1:893322883008:web:cec382858c5c91e5c06225",
+
+    measurementId: "G-B5T77PJF2X"
+
+};
 export function getCurrentUserAuth(){
-    if(auth().currentUser) return auth().currentUser
+    if(firebase.apps.length===0){
+        firebase.initializeApp(firebaseConfig);
+        return null;
+    } else {
+        if(auth().currentUser) return auth().currentUser
+    }
     return null;
 }
